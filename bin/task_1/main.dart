dcadc874d63ec7b4805648b84426cda89a36f112
gvpaleev <gv.paleev@gmail.com>
2024-02-25 12:03:31 -0500

    task_1

---
+++ bin/task_1/main.dart
@@ -0,0 +1,14 @@
+void main() async {
+  // var temperature = 15;
+  // var value = 10;
+  // var dart = 'Dart';
+  // var flutter = 'Flutter';
+
+  int temperature = 15;
+  int value = 10;
+  String dart = 'Dart';
+  String flutter = 'Flutter';
+  print('''Мне нравится изучать $dart и $flutter!
+Сегодня температура $temperature С, а завтра похолодает до ($temperature - 3.1) - ${temperature - 3.1} С.
+$value плюс $value, получится ${value + value}''');
+}

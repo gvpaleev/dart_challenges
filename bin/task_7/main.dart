b8d1497c1b1f99602f33efb65de5443418386159
gvpaleev <gv.paleev@gmail.com>
2024-03-03 06:37:29 -0500

    task_7

--- bin/task_7/main.dart
+++ bin/task_7/main.dart
@@ -1,3 +1,20 @@
+import 'dart:math';
+
 void main() async {
-  print('3' * 3);
+  Cuboid(1, 2, 3);
+
+  Cube(2);
+}
+
+class Cuboid {
+  final int length, width, height;
+  const Cuboid(this.length, this.width, this.height);
+
+  int get volume => length * width * height;
+  int get surfaceArea =>
+      2 * (length * width + length * height + width * height);
+}
+
+class Cube extends Cuboid {
+  Cube(edge) : super(edge, edge, edge);
 }
